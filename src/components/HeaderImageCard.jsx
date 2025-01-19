6f81a78f26408be3ddb83095ae19d3272d937eed
Faizan1421 <faizantayyab37@gmail.com>
2025-01-19 17:51:34 +0500

    updated

--- src/components/HeaderImageCard.jsx
+++ src/components/HeaderImageCard.jsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function ImageCard() {
   return (
-    <div className=" relative flex flex-col items-center justify-center w-[302px] h-[302px] lg:w-[340px] lg:h-[340px] bg-gradient-to-br from-blue-900 to-pink-900 rounded-full cursor-pointer">
-     <div className="z-[-50] absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.72] bg-red-500 rounded-full blur-3xl" />
-    <div className="flex flex-col items-center justify-center w-72 h-72  lg:w-80 lg:h-80 bg-white dark:bg-black rounded-full">
+    <div className=" relative flex flex-col items-center justify-center size-[302px] lg:size-[310px]  bg-gradient-to-br from-blue-900 to-pink-900 rounded-lg cursor-pointer hover:scale-105 transition-all duration-500 ">
+     <div className="z-[-50] absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.90] bg-red-500 rounded-lg blur-2xl  " />
+    <div className="flex flex-col items-center justify-center size-[300px]  lg:size-[300px] bg-white dark:bg-black rounded-lg">
      <Image
        src="https://res.cloudinary.com/dzxywksdm/image/upload/v1733559245/414048569_7141970295891764_5075936731563071177_n_lgiuxi.jpg"
        alt="hero"
        width={1000}
        height={1000}
-       className="opacity-40 mx-auto rounded-full object-cover   "
+       className="opacity-40 h-full w-full  mx-auto rounded-lg object-cover  "
      />
     </div>
     </div>
